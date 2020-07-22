146698d907b14574b8a0d8b4f2d4610a79faa0d5
Stephane THOMAS <stephane.thomas75@gmail.com>
2020-07-22 09:57:25 +0200

    Ajout message erreur
    erreur à la connexion

--- db/connection.php
+++ db/connection.php
@@ -1,8 +1,10 @@
 <?php
+    global $bdd;
+    //$GLOBALS [$bdd];
 try {
-   $db = new PDO('mysql:host=localhost;dbname=GBAF;charset=utf8','root','root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+   $bdd = new PDO('mysql:host=localhost;dbname=GBAF;charset=utf8','root','root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (\Exception $e) {
-    die('Erreur :' . $e->getMessage());
+    die('Erreur de connexion à la base de donnée : ' . $e->getMessage() . ' se référer à la notice d\'utilisation de PHP et MySql.');
 }
 
 ?>
--- functions/functionsql.php
+++ functions/functionsql.php
@@ -3,7 +3,7 @@
 function getUser($username)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT id, lastname, firstname, username, password, question, answer FROM user WHERE username = ?');
+    $req = $bdd->prepare('SELECT id, lastname, firstname, username, password, question, answer FROM user WHERE username = ?');
     $req->execute([$username]);
     $user = $req->fetch();
     return $user;
@@ -12,7 +12,7 @@ function getUser($username)
 function getActors()
 {
     require('db/connection.php');
-    $listActor = $db->query('SELECT * FROM actor');
+    $listActor = $bdd->query('SELECT * FROM actor');
     $actors = $listActor->fetchAll();
     return $actors;
 }
@@ -20,7 +20,7 @@ function getActors()
 function getAnswer($answer, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT id, lastname, firstname, username, password, question, answer FROM user WHERE answer = ? AND username = ?');
+    $req = $bdd->prepare('SELECT id, lastname, firstname, username, password, question, answer FROM user WHERE answer = ? AND username = ?');
     $req->execute([$answer, $username]);
     $answer = $req->fetch();
     return $answer;
@@ -29,7 +29,7 @@ function getAnswer($answer, $username)
 function getActor($idActor)
 {
     require('db/connection.php');
-    $actorInfo = $db->prepare('SELECT * FROM actor WHERE id = ?');
+    $actorInfo = $bdd->prepare('SELECT * FROM actor WHERE id = ?');
     $actorInfo->execute([$idActor]);
     $actor = $actorInfo->fetch();
     return $actor;
@@ -38,7 +38,7 @@ function getActor($idActor)
 function createNewuser($lastname, $firstname, $username, $pass_hache, $question, $answer)
 {
     require('db/connection.php');
-    $req = $db->prepare('INSERT INTO user(lastname, firstname, username, password, question, answer) VALUES(:lastname, :firstname, :username, :password, :question, :answer)');
+    $req = $bdd->prepare('INSERT INTO user(lastname, firstname, username, password, question, answer) VALUES(:lastname, :firstname, :username, :password, :question, :answer)');
     $req->execute([
         'lastname'=> $lastname,
         'firstname' => $firstname,
@@ -52,42 +52,42 @@ function createNewuser($lastname, $firstname, $username, $pass_hache, $question,
 function updatePass($pass_hache, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('UPDATE user SET password = ? WHERE username = ?');
+    $req = $bdd->prepare('UPDATE user SET password = ? WHERE username = ?');
     $req->execute([$pass_hache, $username]);
 }
 
 function updateUsername($newUsername, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('UPDATE user SET username = ? WHERE username = ?');
+    $req = $bdd->prepare('UPDATE user SET username = ? WHERE username = ?');
     $req->execute([$newUsername, $username]);
 }
 
 function updateLastname($newLastname, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('UPDATE user SET lastname = ? WHERE username = ?');
+    $req = $bdd->prepare('UPDATE user SET lastname = ? WHERE username = ?');
     $req->execute([$newLastname, $username]);
 }
 
 function updateFirstname($newFirstname, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('UPDATE user SET firstname = ? WHERE username = ?');
+    $req = $bdd->prepare('UPDATE user SET firstname = ? WHERE username = ?');
     $req->execute([$newFirstname, $username]);
 }
 
 function updateQuestionAnswer($newQuestion, $newAnswer, $username)
 {
     require('db/connection.php');
-    $req = $db->prepare('UPDATE user SET question = ?, answer = ? WHERE username = ?');
+    $req = $bdd->prepare('UPDATE user SET question = ?, answer = ? WHERE username = ?');
     $req->execute([$newQuestion, $newAnswer, $username]);
 }
 
 function getComment($id_actor)
 {
     require('db/connection.php');
-    $listComment = $db->prepare('SELECT comment.comment, DATE_FORMAT(created_at, \'%d/%m/%Y à %Hh%imin%ss\') AS created_at_fr, user.firstname, user.id FROM comment
+    $listComment = $bdd->prepare('SELECT comment.comment, DATE_FORMAT(created_at, \'%d/%m/%Y à %Hh%imin%ss\') AS created_at_fr, user.firstname, user.id FROM comment
         LEFT JOIN user ON comment.user_id = user.id
         WHERE comment.actor_id = ? ORDER BY comment.created_at DESC');
     $listComment->execute([$id_actor]);
@@ -97,7 +97,7 @@ function getComment($id_actor)
 function getVoteByActor($îd_actor)
 {
     require('db/connection.php');
-    $listVotes = $db->prepare('SELECT * FROM vote WHERE actor_id= ?');
+    $listVotes = $bdd->prepare('SELECT * FROM vote WHERE actor_id= ?');
     $listVotes->execute([$îd_actor]);
     return $listVotes;
 }
@@ -105,7 +105,7 @@ function getVoteByActor($îd_actor)
 function getLikeByActor($id_actor)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT COUNT(vote.vote) AS nb_vote FROM vote WHERE actor_id = ? AND vote = 1');
+    $req = $bdd->prepare('SELECT COUNT(vote.vote) AS nb_vote FROM vote WHERE actor_id = ? AND vote = 1');
     $req->execute([$id_actor]);
     $like = $req->fetch();
     return $like;
@@ -114,7 +114,7 @@ function getLikeByActor($id_actor)
 function getDislikeByActor($id_actor)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT COUNT(vote.vote) AS nb_vote FROM vote WHERE actor_id = ? AND vote = 0');
+    $req = $bdd->prepare('SELECT COUNT(vote.vote) AS nb_vote FROM vote WHERE actor_id = ? AND vote = 0');
     $req->execute([$id_actor]);
     $dislike = $req->fetch();
     return $dislike;
@@ -123,7 +123,7 @@ function getDislikeByActor($id_actor)
 function getLikes()
 {
     require('db/connection.php');
-    $listLikes = $db->query('SELECT COUNT(*) AS nb_likes, actor_id FROM vote WHERE vote = 1 GROUP BY actor_id');
+    $listLikes = $bdd->query('SELECT COUNT(*) AS nb_likes, actor_id FROM vote WHERE vote = 1 GROUP BY actor_id');
     $likes = $listLikes->fetchAll();
     return $likes;
 }
@@ -131,7 +131,7 @@ function getLikes()
 function getDislikes()
 {
     require('db/connection.php');
-    $listDislikes = $db->query('SELECT COUNT(*) AS nb_dislikes, actor_id FROM vote WHERE vote = 0 GROUP BY actor_id');
+    $listDislikes = $bdd->query('SELECT COUNT(*) AS nb_dislikes, actor_id FROM vote WHERE vote = 0 GROUP BY actor_id');
     $dislikes = $listDislikes->fetchAll();
     return $dislikes;
 }
@@ -139,7 +139,7 @@ function getDislikes()
 function getCommentExist($id_actor, $id_user)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT comment.comment FROM comment
+    $req = $bdd->prepare('SELECT comment.comment FROM comment
         LEFT JOIN user ON comment.user_id = user.id
         WHERE comment.actor_id = ? AND comment.user_id = ?');
     $req->execute([$id_actor, $id_user]);
@@ -150,7 +150,7 @@ function getCommentExist($id_actor, $id_user)
 function getVoteExist($id_actor, $id_user)
 {
     require('db/connection.php');
-    $req = $db->prepare('SELECT vote.vote FROM vote
+    $req = $bdd->prepare('SELECT vote.vote FROM vote
         LEFT JOIN user ON vote.user_id = user.id
         WHERE vote.actor_id = ? AND vote.user_id = ?');
     $req->execute([$id_actor, $id_user]);
@@ -161,7 +161,7 @@ function getVoteExist($id_actor, $id_user)
 function insertComment($comment, $idActor, $iduser)
 {
     require('db/connection.php');
-    $req = $db->prepare('INSERT INTO comment(comment, created_at, actor_id, user_id) VALUES(:comment, NOW(), :actor_id, :user_id)');
+    $req = $bdd->prepare('INSERT INTO comment(comment, created_at, actor_id, user_id) VALUES(:comment, NOW(), :actor_id, :user_id)');
     $req->execute([
         'comment' => $comment,
         'actor_id' => $idActor,
@@ -172,7 +172,7 @@ function insertComment($comment, $idActor, $iduser)
 function insertLike($vote, $idActor, $iduser)
 {
     require('db/connection.php');
-    $req = $db->prepare('INSERT INTO vote(vote, actor_id, user_id) VALUES(:vote, :actor_id, :user_id)');
+    $req = $bdd->prepare('INSERT INTO vote(vote, actor_id, user_id) VALUES(:vote, :actor_id, :user_id)');
     $req->execute([
         'vote' => $vote,
         'actor_id' => $idActor,
